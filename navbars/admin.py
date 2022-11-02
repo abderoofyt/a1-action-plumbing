a2dcde426aaa1cba8c48830b8e442a1eb6808630
abderoofyt <111700270+abderoofyt@users.noreply.github.com>
2022-11-02 22:30:22 +0200

    Add files via upload

---
+++ navbars/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import About, Counter, Gallery, Navbar, Pricing, Service, Testimonial
+
+admin.site.register(About)
+admin.site.register(Gallery)
+admin.site.register(Navbar)
+admin.site.register(Pricing)
+admin.site.register(Service)
+admin.site.register(Testimonial)
---
+++ navbars/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+
+from firsts.models import Navbar
+from users.models import Filter
+
+# Create your models here.
+class Service(models.Model):
+    nav = models.CharField(max_length=30, null=True, blank=True)
+    navbar = models.ForeignKey(Navbar, on_delete=models.CASCADE, null=True, blank=True)
+    services = models.CharField(max_length=30)
+    details = models.CharField(max_length=300, null=True, blank=True)
+    icon = models.CharField(max_length=30, null=True, blank=True)
+
+    def __str__(self):
+        return self.services
+
+class About(models.Model):
+    background = models.CharField(max_length=30)
+    background_image = models.ImageField(upload_to="photo/", null=True, blank=True)
+
+
+class Gallery(models.Model):
+    nav = models.CharField(max_length=30, null=True, blank=True)
+    navbar = models.ForeignKey(Navbar, on_delete=models.CASCADE, null=True, blank=True)
+    alt = models.CharField(max_length=30)
+    filter = models.ForeignKey(Filter, on_delete=models.CASCADE, null=True, blank=True)
+    photo = models.ImageField(upload_to="photo/", null=True, blank=True)
+
+    def __str__(self):
+        return self.alt
+
+class Testimonial(models.Model):
+    nav = models.CharField(max_length=30, null=True, blank=True)
+    navbar = models.ForeignKey(Navbar, on_delete=models.CASCADE, null=True, blank=True)
+    person = models.CharField(max_length=30)
+    quote = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to="photo/", null=True, blank=True)
+    title = models.CharField(max_length=30, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.person
+
+class Counter(models.Model):
+    nav = models.CharField(max_length=30, null=True, blank=True)
+    navbar = models.ForeignKey(Navbar, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=30)
+    number = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Pricing(models.Model):
+    nav = models.CharField(max_length=30, null=True, blank=True)
+    navbar = models.ForeignKey(Navbar, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=20, null=True, blank=True)
+    features = models.BooleanField()
+    name = models.CharField(max_length=20, null=True, blank=True)
+    featured = models.BooleanField()
+    price = models.CharField(max_length=20)
+    saving = models.CharField(max_length=20, null=True, blank=True)
+    payment = models.CharField(max_length=20, null=True, blank=True)
+
+    item_amount = models.IntegerField(null=True, blank=True)
+    item_quantity = models.CharField(max_length=10, null=True, blank=True)
+    item_name = models.CharField(max_length=30, null=True, blank=True)
+
+    def __str__(self):
+        if self.name:
+            return self.name
+        elif self.title:
+            return self.title
+        else:
+            return self.price
