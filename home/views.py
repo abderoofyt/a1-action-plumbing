c28e1900983e0bd933070b9002b53a85eefe8835
abderoofyt <111700270+abderoofyt@users.noreply.github.com>
2022-11-02 23:04:33 +0200

    Add files via upload

--- home/views.py
+++ home/views.py
@@ -3,10 +3,9 @@ from django.template import loader
 from django.shortcuts import redirect, render, get_object_or_404
 from users.forms import OrderForm
 
-from users.models import Filter, Gallery, Navbar, Order, Pricing, Profile, Service, Social, Counter, Testimonial
-from .models import GeeksModel
-from .forms import GeeksForm
-
+from users.models import Filter, Order, Profile, Social
+from navbars.models import Gallery, Service, Counter, Testimonial, Pricing
+from firsts.models import Navbar
 
 def home(request):
     context = {"data": Profile.objects.filter(user=1), "filters": Filter.objects.all(), 
@@ -56,59 +55,3 @@ def orders_complete(request, id):
             pass
     context['form'] = form
     return render(request, "views/orders_complete.html", context)
-
-def info(request):
-    context = { "dataset": GeeksModel.objects.all()}
-    return render(request, "info.html", context)
-
-
-def profiles(request):
-    context = { "dataset": GeeksModel.objects.all()}
-    return render(request, "profiles.html", context)
-
-
-def create_view(request):
-    context = {}
-    form = GeeksForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        try:
-            return redirect('/crud/list/')
-        except:
-            pass
-    context['form'] = form
-    return render(request, "views/create_view.html", context)
-
-def list_view(request):
-    context = { "dataset": GeeksModel.objects.all()}
-    return render(request, "views/list_view.html", context)
-
-def detail_view(request, id):
-    context_404 = { "data": get_object_or_404(GeeksModel,id=id)}
-    return render(request, "views/detail_view.html", context_404)
-
-def update_view(request, id):
-    context = {}
-    obj = get_object_or_404(GeeksModel,id=id)
-    form = GeeksForm(request.POST or None, instance=obj)
-    if form.is_valid():
-        form.save()
-        try:
-            return redirect('/crud/list/'+id)
-        except:
-            pass
-    context['form'] = form
-    return render(request, "views/update_view.html", context)
-
-def delete_view(request, id):
-    context = {}
-    obj = get_object_or_404(GeeksModel,id=id)
-    form = GeeksForm(request.POST or None, instance=obj)
-    if request.method=="POST":
-        obj.delete()
-        try:
-            return redirect('/crud/list/')
-        except:
-            pass
-    context['form'] = form
-    return render(request, "views/delete_view.html", context)
